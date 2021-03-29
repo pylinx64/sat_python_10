4a96a572936f8f73369f4410e74d38c2db65641f
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-03-29 12:00:22 +0300

    Add files via upload

---
+++ sat_python_10/clicker.py
@@ -0,0 +1,18 @@
+import pyautogui
+
+#                 x    y
+pyautogui.moveTo(250, 658)  # перемещаем мышку 
+
+pyautogui.click()   # кликает
+pyautogui.typewrite('KEK 228')   # воодит текст
+
+'''
+# На свой страх и риск!
+pyautogui.keyDown('Win')     # нажимает клавишу 
+pyautogui.PAUSE = 5        # пауза
+pyautogui.typewrite('Google')
+pyautogui.keyDown('Enter')  # нажимает Enret
+'''
+
+pyautogui.moveTo(250, 658)
+pyautogui.click()
